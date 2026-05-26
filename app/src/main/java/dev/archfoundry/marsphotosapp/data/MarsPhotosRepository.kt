7d4ee69239feb76137869bbb9ac9f852545721ab
Fabian Enrique Suarez Carvajal <77165745+ing-fabiansuarez@users.noreply.github.com>
2026-05-26 17:16:32 -0500

    Insercion de dependencias - creacion del contenedor - OJO aun hay error

--- app/src/main/java/dev/archfoundry/marsphotosapp/data/MarsPhotosRepository.kt
+++ app/src/main/java/dev/archfoundry/marsphotosapp/data/MarsPhotosRepository.kt
@@ -1,14 +1,14 @@
 package dev.archfoundry.marsphotosapp.data
 
-import dev.archfoundry.marsphotosapp.network.MarsApi
+import dev.archfoundry.marsphotosapp.network.MarsApiService
 import dev.archfoundry.marsphotosapp.network.MarsPhoto
 
 interface MarsPhotosRepository{
     suspend fun getMarsPhotos(): List<MarsPhoto>
 }
 
-class NetworkMarsPhotosRepository() : MarsPhotosRepository {
+class NetworkMarsPhotosRepository(private val marsApiService: MarsApiService) : MarsPhotosRepository {
     override suspend fun getMarsPhotos(): List<MarsPhoto> {
-        return MarsApi.retrofitService.getPhotos()
+        return marsApiService.getPhotos()
     }
 }
--- app/src/main/java/dev/archfoundry/marsphotosapp/network/MarsApiService.kt
+++ app/src/main/java/dev/archfoundry/marsphotosapp/network/MarsApiService.kt
@@ -1,26 +1,9 @@
 package dev.archfoundry.marsphotosapp.network
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
-import retrofit2.converter.kotlinx.serialization.asConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL =
-    "https://android-kotlin-fun-mars-server.appspot.com"
 
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-    .baseUrl(BASE_URL)
-    .build()
 
 interface MarsApiService {
     @GET("photos")
     suspend fun getPhotos() : List<MarsPhoto>
-}
-
-object MarsApi {
-    val retrofitService : MarsApiService by lazy {
-        retrofit.create(MarsApiService::class.java)
-    }
 }
