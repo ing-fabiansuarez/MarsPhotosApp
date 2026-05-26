4dd050b7fcb33c5d8511de8af351e9de0ed0e8e8
Fabian Enrique Suarez Carvajal <77165745+ing-fabiansuarez@users.noreply.github.com>
2026-05-26 16:53:34 -0500

    Se creo el data class para deserializar el json y se ajusto el converter de retrofit

--- app/src/main/java/dev/archfoundry/marsphotosapp/network/MarsApiService.kt
+++ app/src/main/java/dev/archfoundry/marsphotosapp/network/MarsApiService.kt
@@ -1,5 +1,8 @@
 package dev.archfoundry.marsphotosapp.network
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
@@ -7,7 +10,7 @@ private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
 
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
     .baseUrl(BASE_URL)
     .build()
 
---
+++ app/src/main/java/dev/archfoundry/marsphotosapp/network/MarsPhoto.kt
@@ -0,0 +1,11 @@
+package dev.archfoundry.marsphotosapp.network
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MarsPhoto(
+    val id: String,
+    @SerialName(value = "img_src")
+    val img_src: String
+)
